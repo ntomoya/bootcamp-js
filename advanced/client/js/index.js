cff2302c03f966aab413c4173987f4f8ee323a35
Tomoya Nakanishi <tomoya_nakanishi@r.recruit.co.jp>
2022-04-07 14:02:10 +0900

    Api: create todo

--- advanced/client/js/index.js
+++ advanced/client/js/index.js
@@ -1,3 +1,5 @@
+const BASE_URL = 'http://localhost:3000'
+
 class Todo {
   #id
   #name
@@ -22,14 +24,40 @@ class Todo {
   }
 }
 
+async function apiRequest(path, { method = 'GET', data }) {
+  if (data !== undefined) {
+    method = 'POST'
+  }
+
+  let res;
+  if (method === 'POST') {
+    res = await fetch(BASE_URL + path, {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+  } else {
+    res = await fetch(BASE_URL + path, { method })
+  }
+  return await res.json()
+}
+
 async function fetchTodoList() {
-  const res = await fetch('http://localhost:3000/todo')
-  const { todoList } = await res.json()
+  const { todoList } = await apiRequest('/todo', { method: 'GET' })
   return todoList.map(todo => new Todo(...todo))
+}
+
+async function createTodo(name) {
+  const data = { name }
+  const { id } = await apiRequest('/todo', { data })
+  return id
 }
 
 const main = async () => {
   console.log(await fetchTodoList())
+  // console.log(await createTodo('hoge'))
 }
 
 main()
