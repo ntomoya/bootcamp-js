02ba67fd9c78b1c06f70ed4c6f3b052a9dd13622
Tomoya Nakanishi <tomoya_nakanishi@r.recruit.co.jp>
2022-04-07 13:42:45 +0900

    Fetch todo list

--- advanced/client/js/index.js
+++ advanced/client/js/index.js
@@ -1,5 +1,35 @@
-const main = () => {
-  console.log("ここにロジックを記述");
-};
+class Todo {
+  #id
+  #name
+  #done
 
-main();
+  constructor(id, name, done = False) {
+    this.#id = id
+    this.#name = name
+    this.#done = done
+  }
+
+  get id() {
+    return this.#id
+  }
+
+  get name() {
+    return this.#name
+  }
+
+  get done() {
+    return this.#done
+  }
+}
+
+async function fetchTodoList() {
+  const res = await fetch('http://localhost:3000/todo')
+  const { todoList } = await res.json()
+  return todoList.map(todo => new Todo(...todo))
+}
+
+const main = async () => {
+  console.log(await fetchTodoList())
+}
+
+main()
